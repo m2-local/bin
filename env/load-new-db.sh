1027b6e7ffe752daa2a6eb9180059827103f961c
Nathan Nobbe <nathan@moxune.com>
2020-07-24 15:02:05 -0600

    Some tweaks to load new db script

--- env/load-new-db.sh
+++ env/load-new-db.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # import a db w/ filename presumed to be relative to the $dkr_sata_mount/exports directory
-echo "Dropping DB & Creating blank DB"
-echo "drop database ${dkr_mysql_db}; create database ${dkr_mysql_db};" | mysql -u "$dkr_mysql_user" --password="$dkr_mysql_pw"
-
-db_file="$1"
-db_path=
-if [ "$db_file" == "load-new-db.sh" ]; then
-    db_file="$2"
-    db_path="${dkr_sata_mount}/exports/$db_file"
-fi
-
+db_file="$2"
+db_path="${dkr_sata_mount}/exports/$db_file"
 if [ ! -f "$db_path" ]; then
     echo "Path to new DB file ${db_path} DNE - bailing"
     exit 2
 fi
+
+echo "Dropping DB"
+echo "drop database ${dkr_mysql_db};" | mysql -u "$dkr_mysql_user" --password="$dkr_mysql_pw"
+echo "Creating blank DB"
+echo "create database ${dkr_mysql_db};" | mysql -u "$dkr_mysql_user" --password="$dkr_mysql_pw"
 
 echo "Loading new database $db_file"
 mysql -u "$dkr_mysql_user" --password="$dkr_mysql_pw" "$dkr_mysql_db" < "$db_path"

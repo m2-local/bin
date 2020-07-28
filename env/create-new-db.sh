b73539e72a42c56ec597a30a6aabd94ad86e3612
Nathan Nobbe <nathan@moxune.com>
2020-07-28 12:16:17 -0600

    Surpress error messaging

--- env/create-new-db.sh
+++ env/create-new-db.sh
@@ -1,2 +1,2 @@
 echo "Creating blank DB"
-echo "create database ${dkr_mysql_db};" | mysql -u "$dkr_mysql_user" --password="$dkr_mysql_pw"
+echo "create database ${dkr_mysql_db};" | mysql -u "$dkr_mysql_user" --password="$dkr_mysql_pw" &>/dev/null
